373ebc3d86aca51a4c955def7af48adf8447a0ec
chamila <balasuriye@gmail.com>
2025-10-26 06:33:45 +0100

    adding first commit

---
+++ first-git-file.ts
@@ -0,0 +1,11 @@
+
+const age: number = 30;
+const isStudent: boolean = true;
+
+console.log(name, age, isStudent);
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(add(1, 2));
