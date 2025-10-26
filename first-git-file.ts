6b8476dc2d1b46ba5a9f93ddcf5737dd36be4f63
chamila <balasuriye@gmail.com>
2025-10-26 07:27:45 +0100

    from first-branch

--- first-git-file.ts
+++ first-git-file.ts
@@ -1,2 +1,12 @@
 
 const age: number = 30;
+const isStudent: boolean = true;
+
+console.log(name, age, isStudent);
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+// adding something // qweqwewqeewqe
+// from first-branch
